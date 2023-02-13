2bd4f7786a7b4c483022ca1a77d6274d0fdc136a
DaniloSsoares <danilo.ssoares201@gmail.com>
2023-02-13 15:04:26 -0300

    Atividade 13-02-23

--- Ativ-PW2/Exercicios-001/Exercicio03.php
+++ Ativ-PW2/Exercicios-001/Exercicio03.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Declarando nome e Idade</title>
 </head>
 <body>
+<?php
+Echo "Nome:_ Danilo Santos Soares <br> Idade: 18 Anos.";
 
+?>
 </body>
 </html>
