655ad4cdc1f0bc16756678fbc4778094cf501e58
DaniloSsoares <danilo.ssoares201@gmail.com>
2023-04-26 14:42:45 -0300

    App dado Atualizado

---
+++ Ativ-PM/Ativ19042/app/src/main/java/com/example/ativ_19_04/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.ativ_19_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+        // rollDice()
+    }
+
+
+
+    private fun rollDice() {
+
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView4)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+
+        val TextF: TextView= findViewById(R.id.text)
+        val resultText: EditText = findViewById(R.id.result_text)
+
+
+        if (diceRoll == resultText.text.toString().toInt()){
+            TextF.text =  " GANHOU!!! ALGUMA COISA "
+        } else {
+            TextF.text  = "PERDEU !!! NÃO É NOVIDADE! "
+        }
+
+
+    }
+
+}
+
+
+
+
+
